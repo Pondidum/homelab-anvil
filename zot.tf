b02063ca58803986cdc531ce6127725382ad9c9e
andy <andy@sammalmaa.fi>
2025-08-31 23:53:25 +0300

    zot: add persistent storage

--- zot.tf
+++ zot.tf
@@ -1,8 +1,23 @@
 
+resource "incus_storage_volume" "zot_storage" {
+  name = "zot"
+  pool = "tank"
+}
+
 resource "incus_instance" "zot" {
   name = "zot"
   type = "container"
   image = "ghcr:project-zot/zot-linux-amd64:v2.1.7"
+
+  device {
+    name = "storage"
+    type = "disk"
+    properties = {
+      path = "/var/lib/registry"
+      source = incus_storage_volume.zot_storage.name
+      pool = "tank"
+    }
+  }
 
   # used for seeding images externally, before the loadbalancer exists
   device {
