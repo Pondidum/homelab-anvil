788564341a89d722c58bc60a879611a44f76daae
andy <andy@sammalmaa.fi>
2025-08-31 23:33:26 +0300

    incus: add zot container registry

---
+++ zot.tf
@@ -0,0 +1,16 @@
+
+resource "incus_instance" "zot" {
+  name = "zot"
+  type = "container"
+  image = "ghcr:project-zot/zot-linux-amd64:v2.1.7"
+
+  # used for seeding images externally, before the loadbalancer exists
+  device {
+    name = "ingress-5000"
+    type = "proxy"
+    properties = {
+      listen = "tcp:0.0.0.0:5000"
+      connect = "tcp:127.0.0.1:5000"
+    }
+  }
+}
